4a886c0349478683b9fbfe0d04665f73f2b562ba
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-27 18:05:18 +0200

    Rewrite boilerplate

--- scalafix-rewrites/src/main/scala/olafurpg/scalafix_template/scalafix/Scalafix_template_1_0.scala
+++ scalafix-rewrites/src/main/scala/olafurpg/scalafix_template/scalafix/Scalafix_template_1_0.scala
@@ -6,6 +6,11 @@ import scala.meta._
 import org.scalameta.logger
 case object Scalafix_template_1_0 extends Rewrite {
   override def rewrite(ctx: RewriteCtx): Patch = {
+    println("!!!!!!!!!")
+    q"def x = 2" match {
+      case q"def $name = 2" => name
+    }
+    val q"$name" = Term.Name("x")
     logger.elem(ctx.tree)
     Patch.empty
   }
--- scalafix-rewrites/src/test/scala/olafurpg/scalafix_template/scalafix/Scalafix_template_1_0Test.scala
+++ scalafix-rewrites/src/test/scala/olafurpg/scalafix_template/scalafix/Scalafix_template_1_0Test.scala
@@ -2,5 +2,6 @@ package olafurpg.scalafix_template.scalafix
 
 import scalafix.testkit._
 class Scalafix_template_1_0Test extends SemanticRewriteSuite() {
-
+  val testDir = "scalafix-rewrites/src/test/resources"
+  DiffTest.fromFile(new java.io.File(testDir)).foreach(runDiffTest)
 }
